a161b04f600af12c4636b43985ca8e88b73cbf42
Jason Brackman <brackman@gmail.com>
2024-12-20 11:30:53 -0800

    Adding day 19 completed.

--- day19.py
+++ day19.py
@@ -1,73 +1,42 @@
-from collections import deque
-from functools import lru_cache
-from typing import Iterator
-
 import aoc
-from aoctypes import Vec2
 
 
-def parse(path: str):
+def parse(path: str) -> tuple[list[str], list[str]]:
     lines = aoc.lines(path)
-    designs = [a.strip() for a in next(lines).split(",")]
-    patterns = [l for l in lines if l]
-    return designs, patterns
+    parts = [a.strip() for a in next(lines).split(",")]
+    goals = [l for l in lines if l]
+    return parts, goals
 
 
-@lru_cache(maxsize=None)
-def possible(filtered: tuple[str, ...], goal: str) -> bool:
+def dfs(parts: list[str], goal: str, seen: dict[str, int]):
     if goal == "":
-        return True
+        return 1
 
-    for f in filtered:
-        if goal.startswith(f):
-            if possible(filtered, goal[len(f) :]):
-                return True
+    if goal in seen:
+        return seen[goal]
 
-    return False
-
-
-@lru_cache(maxsize=None)
-def possibles(filtered: tuple[str, ...], goal: str):
-    if goal == "":
-        yield True
-
-    for f in filtered:
-        if goal.startswith(f):
-            yield from possibles(filtered, goal[len(f) :])
-
-    return False
-
-
-def part01(data):
     c = 0
-    designs, patterns = data
+    for f in parts:
+        if goal.startswith(f):
+            c += dfs(parts, goal[len(f) :], seen)
 
-    for pattern in patterns:
-        filtered = [d for d in designs if d in pattern]
-        c += possible(tuple(filtered), pattern)
-
-    assert c == 260
-
-
-def part02(data):
-    c = 0
-    designs, patterns = data
-
-    for pattern in patterns:
-
-        filtered = [d for d in designs if d in pattern]
-        n = [1 for p in possibles(tuple(filtered), pattern) if p is True]
-        print("PATTERN:", pattern, sum(n))
-        c += sum(n)
-
-    print(c)
+    seen[goal] = c
+    return c
 
 
-def run():
+def part01(parts: list[str], goals: list[str]) -> int:
+    return sum(dfs(parts, goal, {}) > 0 for goal in goals)
+
+
+def part02(parts: list[str], goals: list[str]) -> int:
+    return sum(dfs(parts, goal, {}) for goal in goals)
+
+
+def run() -> None:
     path = r"./data/day19.txt"
-    data = parse(path)
-    part01(data)
-    # part02(data)
+    parts, goals = parse(path)
+    assert part01(parts, goals) == 260
+    assert part02(parts, goals) == 639963796864990
 
 
 if __name__ == "__main__":
